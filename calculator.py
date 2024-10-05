e1eb1db59b5383c255ad2f79f7f0fb9ca89b24ac
tjsqhd157 <tjsqhd157@kyonggi.ac.kr>
2024-10-05 21:30:52 +0900

    Upgrade Calculator

--- calculator.py
+++ calculator.py
@@ -4,6 +4,15 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "0으로 나눌 수 없습니다."
+    else:
+        return a / b
+
 # 사용자 입력 받기
 num1 = float(input("첫 번째 숫자를 입력하세요: "))
 num2 = float(input("두 번째 숫자를 입력하세요: "))
@@ -11,3 +20,5 @@ num2 = float(input("두 번째 숫자를 입력하세요: "))
 # 결과 출력
 print(f"{num1} + {num2} = {add(num1, num2)}")
 print(f"{num1} - {num2} = {subtract(num1, num2)}")
+print(f"{num1} * {num2} = {multiply(num1, num2)}")
+print(f"{num1} / {num2} = {divide(num1, num2)}")
